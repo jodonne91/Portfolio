941f6e4390faf5331d2134e81203cc0c28507edb
jodonne91 <jodonne91@gmail.com>
2015-06-14 14:20:23 -0400

    removed home

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -9,9 +9,9 @@
  */
 angular.module('portfolioApp')
   .controller('GalleryCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.examples = [
+    	{
+    		
+    	}
+    ]
   });
